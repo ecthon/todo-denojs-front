530d4793497cc432c96cf2c52d958fcf44f16b65
ecthon <ecthon@gmailcom>
2025-06-18 06:15:54 -0400

    refactor: improve button styling and update background colors in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,24 +14,24 @@ export default function Home() {
               <span className="text-sm text-zinc-500">Progress of your activities.</span>
             </div>
           </div>
-          <button className="bg-zinc-950 py-3 rounded-lg text-white cursor-pointer">Add new task</button>
+          <button className="bg-zinc-950 p-3 rounded-lg text-white cursor-pointer">Add new task</button>
         </div>
         {/* Task Open */}
         <div className="flex flex-col space-y-4 border bg-white p-4 rounded-2xl max-w-md w-full">
           <h1 className="text-zinc-500 text-xs font-semibold">OPEN</h1>
-          <div className="flex justify-between border border-zinc-200 p-3 rounded-lg bg-zinc-100">
+          <div className="flex justify-between border border-zinc-200 p-3 rounded-lg bg-zinc-50">
             <div className="flex items-center space-x-2">
               <button className="group flex items-center justify-center cursor-pointer bg-white rounded-full">
-                <Circle className="w-5 h-5 text-zinc-600 group-hover:text-zinc-950 bg-white rounded-full transition-colors" />
+                <Circle className="w-5 h-5 text-zinc-400 group-hover:text-zinc-950 bg-white rounded-full transition-colors" />
               </button>
               <p className="text-zinc-950 text-sm">Create a API withe Deno.js</p>
             </div>
-            <div className="flex items-center justify-center gap-0.5">
+            <div className=" flex items-center justify-center gap-0.5">
               <button className="group flex items-center cursor-pointer justify-center p-1.5 rounded-md hover:bg-zinc-200 transition-colors duration-200">
-                <Edit className="w-4 h-4 text-zinc-700 group-hover:text-zinc-900 transition-colors" />
+                <Edit className="w-4 h-4 text-zinc-400 group-hover:text-zinc-900 transition-colors" />
               </button>
               <button className="group flex items-center cursor-pointer justify-center p-1.5 rounded-md hover:bg-zinc-200 transition-colors duration-200">
-                <LucideTrash2 className="w-4 h-4 text-zinc-700 group-hover:text-zinc-900 transition-colors" />
+                <LucideTrash2 className="w-4 h-4 text-zinc-400 group-hover:text-zinc-900 transition-colors" />
               </button>
             </div>
           </div>
@@ -40,16 +40,16 @@ export default function Home() {
         {/* Task Close */}
         <div className="flex flex-col space-y-4 border bg-white p-4 rounded-2xl max-w-md w-full">
           <h1 className="text-zinc-500 text-xs font-semibold">CLOSE</h1>
-          <div className="flex justify-between border border-zinc-200 p-3 rounded-lg bg-zinc-100">
+          <div className="flex justify-between border border-zinc-200 p-3 rounded-lg bg-zinc-50">
             <div className="flex items-center space-x-2">
               <button className="flex items-center justify-center cursor-pointer bg-white rounded-full">
-                <CircleCheck className="w-5 h-5 text-zinc-950 bg-white rounded-full" />
+                <CircleCheck className="w-5 h-5 text-zinc-400 hover:text-zinc-950 bg-white rounded-full" />
               </button>
               <p className="text-zinc-400 line-through text-sm text-">Create a API withe Deno.js</p>
             </div>
             <div className="flex items-center justify-center gap-0.5">
               <button className="group flex items-center cursor-pointer justify-center p-1.5 rounded-md hover:bg-zinc-200 transition-colors duration-200">
-                <LucideTrash2 className="w-4 h-4 text-zinc-700 group-hover:text-zinc-900 transition-colors" />
+                <LucideTrash2 className="w-4 h-4 text-zinc-400 group-hover:text-zinc-900 transition-colors" />
               </button>
             </div>
           </div>
